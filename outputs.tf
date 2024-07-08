aefe027ae7ab8d3c7a02e19156dc774ea9200c57
Marin Petkov <marin.petkov@hashicorp.com>
2024-07-08 12:04:20 +0300

    no op

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,5 @@ output "public_VM_address" {
   description = "VM public ip address"
   value       = azurerm_linux_virtual_machine.public_vm.public_ip_address
 }
+
+## No op - test policy cost estimate HCP terraform US
