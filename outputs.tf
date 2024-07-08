ea70473990065b808110a2c2e2188d8fec8624d4
Marin Petkov <marin.petkov@hashicorp.com>
2024-07-08 13:51:31 +0300

    soft mandatroy successful

--- outputs.tf
+++ outputs.tf
@@ -3,9 +3,6 @@ output "public_VM_address" {
   value       = azurerm_linux_virtual_machine.public_vm.public_ip_address
 }
 
-## No op - test policy cost estimate HCP terraform US (soft mandatory)
-## No op hard mandatory policy
-## no op - successful policy for eu and us
-## no op - successful policy set advisory mode us and eu
 ## no op - fail policy set advisory mode us and eu
 ## no op - successful policy set advisory mode us and eu
+## no op - successful policy set soft mandatory for us and eu
