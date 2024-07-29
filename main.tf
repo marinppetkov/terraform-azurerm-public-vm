fcfbab9947257111e850530ea21cfd779faa17a3
Marin Petkov <marin.petkov@hashicorp.com>
2024-07-29 15:29:10 +0300

    formatting

--- main.tf
+++ main.tf
@@ -69,8 +69,8 @@ resource "azurerm_linux_virtual_machine" "public_vm" {
     publisher = "Canonical"
     # offer     = "0001-com-ubuntu-server-jammy"
     # sku       = "22_04-lts"
-    offer = "0001-com-ubuntu-server-focal"
-    sku="20_04-lts"
+    offer   = "0001-com-ubuntu-server-focal"
+    sku     = "20_04-lts"
     version = "latest"
   }
 }
--- variables.tf
+++ variables.tf
@@ -4,11 +4,24 @@ variable "location" {
   default = "West Europe"
 }
 
-variable "data_disks"{
-    type = map(object({
-      name = string
-      disk_size_gb = number
-      lun = number
-    }))
-    default = {}
+variable "data_disks" {
+  type = map(object({
+    name         = string
+    disk_size_gb = number
+    lun          = number
+  }))
+  default = {
+    data_disks = {
+      disk01 = {
+        name         = "disk1"
+        disk_size_gb = 10
+        lun          = 10
+      }
+      disk02 = {
+        name         = "disk2"
+        disk_size_gb = 10
+        lun          = 20
+      }
+    }
+  }
 }
